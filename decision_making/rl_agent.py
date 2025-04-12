d1b411e347c1dbf5d2d30dbf0828bd283efb0dec
a2751012962 <71187843+a2751012962@users.noreply.github.com>
2025-04-11 22:20:32 -0500

    Add files via upload

---
+++ decision_making/rl_agent.py
@@ -0,0 +1,93 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from collections import deque
+import random
+
+class DQN(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(DQN, self).__init__()
+        self.fc1 = nn.Linear(input_size, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, output_size)
+        
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        return self.fc3(x)
+
+class RLAgent:
+    def __init__(self, state_size, action_size, learning_rate=0.001, gamma=0.99):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=10000)
+        self.gamma = gamma
+        self.epsilon = 1.0
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = learning_rate
+        
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = DQN(state_size, action_size).to(self.device)
+        self.target_model = DQN(state_size, action_size).to(self.device)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
+        
+    def remember(self, state, action, reward, next_state, done):
+        """存储经验到记忆回放池"""
+        self.memory.append((state, action, reward, next_state, done))
+        
+    def act(self, state):
+        """根据当前状态选择动作"""
+        if random.random() <= self.epsilon:
+            return random.randrange(self.action_size)
+        
+        state = torch.FloatTensor(state).unsqueeze(0).to(self.device)
+        act_values = self.model(state)
+        return torch.argmax(act_values).item()
+    
+    def replay(self, batch_size):
+        """从记忆回放池中学习"""
+        if len(self.memory) < batch_size:
+            return
+        
+        minibatch = random.sample(self.memory, batch_size)
+        states = torch.FloatTensor([i[0] for i in minibatch]).to(self.device)
+        actions = torch.LongTensor([i[1] for i in minibatch]).to(self.device)
+        rewards = torch.FloatTensor([i[2] for i in minibatch]).to(self.device)
+        next_states = torch.FloatTensor([i[3] for i in minibatch]).to(self.device)
+        dones = torch.FloatTensor([i[4] for i in minibatch]).to(self.device)
+        
+        current_q_values = self.model(states).gather(1, actions.unsqueeze(1))
+        next_q_values = self.target_model(next_states).max(1)[0].detach()
+        target_q_values = rewards + (1 - dones) * self.gamma * next_q_values
+        
+        loss = nn.MSELoss()(current_q_values.squeeze(), target_q_values)
+        
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+            
+    def update_target_model(self):
+        """更新目标网络"""
+        self.target_model.load_state_dict(self.model.state_dict())
+        
+    def save(self, filename):
+        """保存模型"""
+        torch.save({
+            'model_state_dict': self.model.state_dict(),
+            'target_model_state_dict': self.target_model.state_dict(),
+            'optimizer_state_dict': self.optimizer.state_dict(),
+            'epsilon': self.epsilon
+        }, filename)
+        
+    def load(self, filename):
+        """加载模型"""
+        checkpoint = torch.load(filename)
+        self.model.load_state_dict(checkpoint['model_state_dict'])
+        self.target_model.load_state_dict(checkpoint['target_model_state_dict'])
+        self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        self.epsilon = checkpoint['epsilon'] 
